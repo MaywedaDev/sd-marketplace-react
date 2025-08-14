a5a9f2f4e6348d2ddce2b0420135680d1185acdc
Enyo Onuche <enyoonuche1@gmail.com>
2025-08-14 22:53:36 +0100

    Updated Code

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,9 +5,7 @@ import { Outlet } from "react-router-dom";
 const AppLayout: React.FC = () => {
   return (
     <div className="min-h-full flex flex-col w-full">
-      <header>
-        <nav></nav>
-      </header>
+      <header></header>
       <main className="flex-1 max-h-full flex flex-col">
         <div className="w-full h-full overflow-y-scroll">
           <Outlet />
